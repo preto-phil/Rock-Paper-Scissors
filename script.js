ec833b3814006626286b80c58efa3bd054e5debb
Philip Pretorius <philippretorius.79@gmail.com>
2022-09-13 16:42:04 +0200

    Progress 1: Pseudocode

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 //// Rock Paper Scissors
 
+//// Get output that computer is going to use
+
+function getComputerChoice() {
+    // Randomly return option btw r,p & s
+        // return 'Rock'
+        // return 'Paper'
+        // return 'Scissors'
+    // return random(['Rock', 'Paper', 'Scissors']);
+
+}
+
+//// Get playerSelection
+
+let playerSelection = prompt('make your choice');
+
+
+//// Write a function that plays RPS
+
+function play(playerSelection, getComputerChoice) {
+
+    // if (r v s || p v r || s v p)
+        // return You win
+
+    // if (r v r || p v p || s v s)
+        // return You draw
+
+    // if (r v p || p v s || s v r)
+        // return You lose
+
+
+}
