5006aca59efb77fa15b7731c18b5692b4a0b185c
Philip Pretorius <philippretorius.79@gmail.com>
2022-09-13 16:52:07 +0200

    Stage 1.1: Pseudocode Progress

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ function getComputerChoice() {
 let playerSelection = prompt('make your choice');
 
 
-//// Write a function that plays RPS
+//// Write a function that plays a round of RPS
 
-function play(playerSelection, getComputerChoice) {
+function playRound(playerSelection, getComputerChoice) {
 
     // if (r v s || p v r || s v p)
         // return You win
@@ -28,6 +28,25 @@ function play(playerSelection, getComputerChoice) {
 
     // if (r v p || p v s || s v r)
         // return You lose
+
+    // NB MAKE PARAMETERS CASE INSENSITIVE
+
+}
+
+//// Write a function game() that plays 5 times and that records the score
+
+function game() {
+    // Write for loop to repeat game for 5 rounds
+        // Call playRound 5 times
+    // for () {};
+    // How to record score?
+        // if win then what
+            // console.log()
+        // if lose then what
+            // console.log()
+        // if draw then what
+            // console.log()
+    // After 5 rounds, how to declare the winner?
 
 
 }
