213ec72d5215f9d589b3d3111e0026735bb01246
Philip Pretorius <philippretorius.79@gmail.com>
2022-09-14 13:17:15 +0200

    playRound progress

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //// Rock Paper Scissors
 
-let myArray = ['Rock','Paper','Scissor'];
+let myArray = ['Rock','Paper','Scissors'];
 let randomArray = Math.floor(Math.random() * myArray.length);
 
 //// Get output that computer is going to use
@@ -14,29 +14,48 @@ console.log(getComputerChoice(myArray));
 
 //// Get playerSelection
 
-let playerSelection = prompt('make your choice');
+let playerSelection = prompt('Rock, Paper or Scissors?');
+
+console.log(playerSelection)
 
 //// Get computerSelection
 
-let computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice(myArray);
+
+console.log(computerSelection)
 
 //// Write a function that plays a round of RPS
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
 
     // if (r v s || p v r || s v p)
         // return You won
 
+    if (playerSelection == 'Rock' && computerSelection == 'Scissors' || playerSelection == 'Paper' && computerSelection == 'Rock' || playerSelection == 'Scissors' && computerSelection == 'Paper') {
+        return "You Won!";
+    };
+
     // if (r v p || p v s || s v r)
         // return You lost
+
+    if (playerSelection == 'Rock' && computerSelection == 'Paper' || playerSelection == 'Paper' && computerSelection == 'Scissors' || playerSelection == 'Scissors' && computerSelection == 'Rock') {
+        return "You Lost!";
+    };
 
     // if (r v r || p v p || s v s)
         // return Draw
         // repeat playRound
 
+    if (playerSelection == 'Rock' && computerSelection == 'Rock' || playerSelection == 'Paper' && computerSelection == 'Paper' || playerSelection == 'Scissors' && computerSelection == 'Scissors') {
+        return "Draw!";
+    };
+
     // NB MAKE PARAMETERS CASE INSENSITIVE
+        // \i
 
 }
+
+console.log(playRound());
 
 //// Write a function game() that plays 5 times and that records the score
 
