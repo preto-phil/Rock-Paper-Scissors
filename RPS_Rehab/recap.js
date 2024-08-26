0f1cad48d8934f445a9d8d73fa2b344e597f868f
preto-phil <104874252+preto-phil@users.noreply.github.com>
2024-08-26 23:46:22 +0200

    Additions: RPS_Rehab directory for recap and practicing purposes, files and JS Pseudocode

---
+++ RPS_Rehab/recap.js
@@ -0,0 +1,31 @@
+
+/* Pseudocode: Step by Step Solution 
+
+#1 - Make choice - player
+  
+  - click on one of three div's
+  - div element contains event listener for click
+  - equal choice to div class Name / text
+  - return / display player choice
+  - eventlistener calls functions that generate cpu choice, checks winning conditions and keeps score
+
+#2 - Generate cpu choice
+
+  - create function that generates random number between 1 and 3
+  - assign variables R,P,S to numbers 1 - 3
+  - return / display cpu choice
+
+#3 - Compare player choice to winning conditions
+
+  - if x,y,z then win conditions
+  - else if a,b,c then draw conditions
+  - else lose
+  - return / display result
+
+#4 - Keep score of results
+
+  - count wins and loses
+  - return / display score
+
+*/
+
