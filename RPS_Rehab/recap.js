6f4a2e6edb46c9d250938387d5a4aa01f8b979fa
preto-phil <104874252+preto-phil@users.noreply.github.com>
2024-08-27 01:18:12 +0200

    JS: console program progress

--- RPS_Rehab/recap.js
+++ RPS_Rehab/recap.js
@@ -1,31 +1,102 @@
 
 /* Pseudocode: Step by Step Solution 
 
-#1 - Make choice - player
-  
-  - click on one of three div's
-  - div element contains event listener for click
-  - equal choice to div class Name / text
-  - return / display player choice
-  - eventlistener calls functions that generate cpu choice, checks winning conditions and keeps score
+Javascript
 
-#2 - Generate cpu choice
+  #1 - Make choice - player
+    
+    - click on one of three div's
+    - div element contains event listener for click
+    - equal choice to div class Name / text
+    - return / display player choice
+    - eventlistener calls functions that generate cpu choice, checks winning conditions and keeps score
 
-  - create function that generates random number between 1 and 3
-  - assign variables R,P,S to numbers 1 - 3
-  - return / display cpu choice
+  #2 - Generate cpu choice
 
-#3 - Compare player choice to winning conditions
+    - create function that generates random number between 1 and 3
+    - assign variables R,P,S to numbers 1 - 3
+    - return / display cpu choice
 
-  - if x,y,z then win conditions
-  - else if a,b,c then draw conditions
-  - else lose
-  - return / display result
+  #3 - Compare player choice to winning conditions
 
-#4 - Keep score of results
+    - if x,y,z then win conditions
+    - else if a,b,c then draw conditions
+    - else lose
+    - return / display result
 
-  - count wins and loses
-  - return / display score
+  #4 - Keep score of results
+
+    - count wins and loses
+    - return / display score
+
+HTML
+
+  - Display buttons
+  - Display player choice
+  - 
 
 */
 
+// Console version
+
+let player_score = 0;
+let cpu_score = 0;
+
+function playerChoice() {
+  let p_choice = prompt('Rock, Paper, Scissors?', 'rock');
+  console.log('Player: ' + p_choice)
+  return p_choice;
+}
+
+function computerChoice() {
+  let randomNumber = Math.ceil(Math.random() * 3);
+  let c_choice;
+  if (randomNumber === 1) {
+    c_choice = 'rock';
+  } else if (randomNumber === 2) {
+    c_choice = 'paper';
+  } else {
+    c_choice = 'scissors';
+  }
+  console.log('Computer: ' + c_choice);
+  return c_choice;
+};
+
+function checkWin(p_choice = playerChoice(), c_choice = computerChoice()) {
+  let result;
+  if (
+    p_choice == 'rock' && c_choice == 'scissors' ||
+    p_choice == 'paper' && c_choice == 'rock' ||
+    p_choice == 'scissors' && c_choice == 'paper'
+  ) {
+    result = 'win';
+  } else if (
+    p_choice == 'rock' && c_choice == 'rock' ||
+    p_choice == 'paper' && c_choice == 'paper' ||
+    p_choice == 'scissors' && c_choice == 'scissors'
+  ) {
+    result = 'draw';
+  } else {
+    result = 'lose';
+  }
+  console.log("Outcome: " + result);
+  return result;
+}
+
+function score() {
+  let result = checkWin();
+  if (result == 'win') {
+    ++player_score;
+  } 
+  if (result == 'lose') {
+    ++cpu_score;
+  }
+  console.log('Player score: ' + player_score);
+  console.log('CPU score: ' + cpu_score);
+  if (player_score === 5) {
+    console.log('Game over. You Win!');
+  }
+  if (cpu_score === 5) {
+    console.log('Game over. You Lose!');
+  }
+}
